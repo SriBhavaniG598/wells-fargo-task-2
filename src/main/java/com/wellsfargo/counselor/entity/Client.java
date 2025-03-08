395d391c1557819529cc790105244389c35a2407
sri <22wh1a0598@bvrithyderabad.edu.in>
2025-03-08 20:51:38 +0530

    Implemented entities for Client, Portfolio, and Security

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,81 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue()
+    private long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "advisorId", nullable = false) // Establishing a relationship with Advisor
+    private Advisor advisor;
+
+
+
+    protected Client() {
+        // Default constructor for JPA
+    }
+
+    public Client(String firstName, String lastName, String email, Advisor advisor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.advisor = advisor;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+
+}
